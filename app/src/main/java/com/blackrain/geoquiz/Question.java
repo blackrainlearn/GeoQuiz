a0e96c221fe4ed6b6d7dcfb1eca7cf9a99210627
renchaojian_home <254777599@qq.com>
2018-01-22 22:04:40 +0800

    add question class

---
+++ app/src/main/java/com/blackrain/geoquiz/Question.java
@@ -0,0 +1,17 @@
+package com.blackrain.geoquiz;
+
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+
+public class Question
+{
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue)
+    {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
