d649dd530bfabd650b4b6f5638ade98a84d5ed6b
blackrainlearn <2743558475@qq.com>
2018-01-23 10:57:59 +0800

    loop question

--- app/src/main/java/com/blackrain/geoquiz/Question.java
+++ app/src/main/java/com/blackrain/geoquiz/Question.java
@@ -6,6 +6,27 @@ package com.blackrain.geoquiz;
 
 public class Question
 {
+
+    public int getTextResId()
+    {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId)
+    {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue()
+    {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue)
+    {
+        mAnswerTrue = answerTrue;
+    }
+
     private int mTextResId;
     private boolean mAnswerTrue;
 
